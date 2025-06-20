eaf4c263ee9c98ebb0028d26be6898791d9a7950
Sejal Agarwal <sejalagarwal@Sejals-MacBook-Air.local>
2025-06-20 08:06:12 -0700

    small fixes

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -18,7 +18,7 @@ export function Contact() {
 
   return (
     <Box
-    className="fade-in"
+      className="fade-in"
       component="footer"
       id="contact"
       sx={{
@@ -30,54 +30,56 @@ export function Contact() {
         <Typography variant="h4" gutterBottom sx={{ fontWeight: 700 }}>
           Contact Me!
         </Typography>
-
-        <Box sx={{ pl: 1 }}>
-          <Stack
-            direction={{ xs: "column", sm: "row" }}
-            spacing={4}
-            alignItems={{ xs: "flex-start", sm: "center" }}
-          >
-            <Stack direction="row" spacing={1} alignItems="center">
-              <EmailIcon color="primary" />
-              <Link
-                href="mailto:sejalagarwal321@gmail.com"
-                underline="hover"
-                color="text.primary"
-                sx={{ fontWeight: 500 }}
-              >
-                <Typography variant="body1">
-                  sejalagarwal321@gmail.com
-                </Typography>
-              </Link>
-            </Stack>
-            <Stack direction="row" spacing={1} alignItems="center">
-              <PhoneIcon color="primary" />
-              <Link
-                href="tel:+12508181343"
-                underline="hover"
-                color="text.primary"
-                sx={{ fontWeight: 500 }}
-              >
-                <Typography variant="body1">(250)-818-1343</Typography>
-              </Link>
-            </Stack>
-            <Stack direction="row" spacing={1} alignItems="center">
-              <LinkedInIcon color="primary" />
-              <Link
-                href="https://www.linkedin.com/in/sejal-agarwal-132572205/"
-                target="_blank"
-                rel="noopener"
-                underline="hover"
-                color="text.primary"
-                sx={{ fontWeight: 500 }}
-              >
-                <Typography variant="body1">
-                  linkedin.com/in/sejal-agarwal
-                </Typography>
-              </Link>
-            </Stack>
+        <Stack
+          direction={{ xs: "column", sm: "row" }}
+          spacing={4}
+          alignItems={{ xs: "flex-start", sm: "center" }}
+        >
+          {/* Email */}
+          <Stack direction="row" spacing={2} alignItems="center">
+            <EmailIcon color="primary" sx={{ mr: 0 }} />
+            <Link
+              href="mailto:sejalagarwal321@gmail.com"
+              underline="hover"
+              color="text.primary"
+              sx={{ fontWeight: 500 }}
+            >
+              <Typography variant="body1">
+                sejalagarwal321@gmail.com
+              </Typography>
+            </Link>
           </Stack>
-        </Box>
+
+          {/* Phone */}
+          <Stack direction="row" spacing={2} alignItems="center">
+            <PhoneIcon color="primary" sx={{ mr: 0 }} />
+            <Link
+              href="tel:+12508181343"
+              underline="hover"
+              color="text.primary"
+              sx={{ fontWeight: 500 }}
+            >
+              <Typography variant="body1">(250)-818-1343</Typography>
+            </Link>
+          </Stack>
+
+          {/* LinkedIn */}
+          <Stack direction="row" spacing={2} alignItems="center">
+            <LinkedInIcon color="primary" sx={{ mr: 0 }} />
+            <Link
+              href="https://www.linkedin.com/in/sejal-agarwal-132572205/"
+              target="_blank"
+              rel="noopener"
+              underline="hover"
+              color="text.primary"
+              sx={{ fontWeight: 500 }}
+            >
+              <Typography variant="body1">
+                linkedin.com/in/sejal-agarwal
+              </Typography>
+            </Link>
+          </Stack>
+        </Stack>
       </Container>
     </Box>
   );
--- src/app/components/WorkTimeline.tsx
+++ src/app/components/WorkTimeline.tsx
@@ -173,7 +173,7 @@ export function WorkTimeline() {
         style={{
           position: "absolute",
           top: barTop,
-          left: isSmall ? `${trackX - 1}px` : "calc(50% - 1px)",
+          left: isSmall ? `${trackX}px` : "calc(50% - 1px)",
           width: 2,
           height: barHeight,
           backgroundColor: theme.palette.primary.main,
